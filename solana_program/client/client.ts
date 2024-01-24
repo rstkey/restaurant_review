5fa4ba2b5324ac591baa0d4f58f04af08ac8a786
rust-solman <lussi.petelius@gmail.com>
2024-01-24 17:41:00 +0530

    Implement the restaurant review Solana program

--- solana_program/client/client.ts
+++ solana_program/client/client.ts
@@ -1,4 +1,10 @@
+import * as web3 from "@solana/web3.js";
+// Manually initialize variables that are automatically defined in Playground
+const PROGRAM_ID = new web3.PublicKey("EH92vyc4PaqfiH7Cfr1e2nXD5KHX73eHTunXMuJEESQZ");
+const connection = new web3.Connection("https://api.devnet.solana.com", "confirmed");
+const wallet = { keypair: web3.Keypair.generate() };
+
 // Client
-console.log("My address:", pg.wallet.publicKey.toString());
-const balance = await pg.connection.getBalance(pg.wallet.publicKey);
+console.log("My address:", wallet.keypair.publicKey.toString());
+const balance = await connection.getBalance(wallet.keypair.publicKey);
 console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
